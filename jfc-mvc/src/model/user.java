41379d387a703bc9d3d26e62682b873fc9385e61
Akbar Nur iman <122083444+Akbarni@users.noreply.github.com>
2024-12-13 20:19:33 +0700

    Materi jfc-mvc

---
+++ jfc-mvc/src/model/user.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author AKBAR NUR IMAN
+ */
+package model;  
+
+public class user {  
+    private int id;  
+    private String name;  
+    private String email;  
+
+    // Getters dan Setters  
+    public int getId() { return id; }  
+    public void setId(int id) { this.id = id; }  
+    public String getName() { return name; }  
+    public void setName(String name) { this.name = name; }  
+    public String getEmail() { return email; }  
+    public void setEmail(String email) { this.email = email; }  
+}
